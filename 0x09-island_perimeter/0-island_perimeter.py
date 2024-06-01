33d21bdd8709a379650df08c3637e6a22e64db1d
GreenMoCh <125460183+GreenMoCh@users.noreply.github.com>
2024-06-01 23:25:28 +0100

    0x09. Island Perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Island perimeter"""
+
+
+def ismand_perimeter(grid):
+    """
+    Calculates the perimeter of an island
+    """
+    perimeter = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if (grid[i][j] == 1):
+                if (i <= 0 or grid[i - 1][j] == 0):
+                    perimeter = perimeter + 1
+
+                if (i >= len(grid) - 1 or grid[i + 1][j] == 0):
+                    perimeter = perimeter + 1
+
+                if (j <= 0 or grid[i][j - 1] == 0):
+                    perimeter = perimeter + 1
+
+                if (j >= len(grid[i]) - 1 or grid[i][j + 1] == 0):
+                    perimeter = perimeter + 1
+
+    return perimeter
+    
