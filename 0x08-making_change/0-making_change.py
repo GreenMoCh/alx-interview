e8991e618567545a35cb2af7fd0d73b3ad232bd1
GreenMoCh <125460183+GreenMoCh@users.noreply.github.com>
2024-06-03 16:29:42 +0100

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,19 +6,14 @@ def makeChange(coins, total):
     """
     Determine the fewest number of coins needed to meet a given amount total
     """
-    if not coins or coins is None:
-        return -1
     if total <= 0:
         return 0
 
-    db = [total + 1] * (total + 1)
+    db = [float('inf')] * (total + 1)
     db[0] = 0
 
     for coin in coins:
         for amount in range(coin, total + 1):
             db[amount] = min(db[amount], db[amount - coin] + 1)
 
-    if db[total] <= total:
-        return db[total]
-    else:
-        return -1
+    return db[total] if db[total} != float('inf') else -1
